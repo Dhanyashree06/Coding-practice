216000497ea1fc413287fe03f8294d40d0e5207e
Dhanyashree <dhanyashree316@gmail.com>
2026-07-15 11:36:46 +0530

    Update changes

--- day3.java
+++ day3.java
@@ -1,3 +1,4 @@
+/************
 import java.util.Scanner;
 
 public class day3{
@@ -18,7 +19,41 @@ public class day3{
 
             n = n / 10;
         }
-
+        sc.close();
         System.out.println(count);
+    }
+}
+****************/
+
+import java.util.Scanner;
+
+public class day3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int temp = n;
+        int sum = 0;
+
+        while (temp > 0) {
+
+            int digit = temp % 10;
+            int fact = 1;
+
+            for (int i = 1; i <= digit; i++) {
+                fact = fact * i;
+            }
+
+            sum = sum + fact;
+            temp = temp / 10;
+        }
+
+        if (sum == n)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+        sc.close();
     }
 }
