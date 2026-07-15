558ec7cc08e8209df14f9c28c2876b0353e7836c
Dhanyashree S <dhanyashree316@gmail.com>
2026-07-15 11:36:58 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -1,6 +1,51 @@
 import java.util.Scanner;
 
 public class day1 {
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            long N = sc.nextLong();
+            int count = 0;
+
+            while (N > 0) {
+                if ((N & 1) == 1)
+                    count++;
+
+                N = N >> 1;
+            }
+
+            if (isPrime(count))
+                System.out.println("Binary prime");
+            else
+                System.out.println("-1");
+        }
+
+        sc.close();   // Close AFTER all test cases
+    }
+}
+
+
+/**********************
+import java.util.Scanner;
+
+public class day1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -12,3 +57,4 @@ public class day1 {
         System.out.println(moves);
     }
 }
+******************/
